8a1ca00bf21630050dc5ddcd0335acf760074e22
LCHNAN <yanchu17@outlook.com>
2023-04-08 17:55:39 +0800

    Project6 Qusetion1 ReDo

--- Project6_1/main.cpp
+++ Project6_1/main.cpp
@@ -2,36 +2,40 @@
     数重载为非成员、非友元的普通函数。编程序，求两个复数之和。
 */
 
-#include <iostream>
-using namespace std;
-class Complex{
-public:
-    Complex(){
-        real = 0;
-        imag = 0;
+#include<iostream>
+    using namespace std;
+    class Complex
+    {public:
+        Complex(){real=0;imag=0;}
+        Complex(double r,double i)
+        {real=r;imag=i;}
+        void get_value();
+        void display();
+//    private:
+        double real;
+        double imag;
+    };
+    Complex operator+(Complex &c1,Complex &c2)
+    {
+        return Complex(c1.real+c2.real,c1.imag+c2.imag);
     }
-    Complex(double r,double i){
-        real = r;
-        imag = i;
+    void Complex::get_value()
+    {
+        cin>>real>>imag;
     }
-    void display(){
-        cout<<real<<","<<imag<<endl;
+
+    void Complex::display()
+    {
+        cout<<"("<<real<<","<<imag<<"i)"<<endl;
     }
-    Complex operator+(Complex &c1);
-private:
-    double real;
-    double imag;
-};
-Complex Complex::operator+(Complex &c1) {
-    Complex c;
-    c.real = c1.real + this->real;
-    c.imag = c1.imag + this->imag;
-    return c;
-}
-int main() {
-    Complex c1(3,4);
-    Complex c2(5,6);
-    Complex c;
-    c = c1.operator+(c2);
-    c.display();
-}
+    int main()
+    {
+        Complex c1,c2,c3;
+        c1.get_value();
+        c2.get_value();
+        cout<<"c1=";c1.display();
+        cout<<"c2=";c2.display();
+        c3=c1+c2;
+        cout<<"c1+c2=";    c3.display();
+        return 0;
+    }
