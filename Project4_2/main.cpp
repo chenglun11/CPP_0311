2ea12bc2aeb7eb81a6bd3526f5646051759b2197
LCHNAN <yanchu17@outlook.com>
2023-04-08 18:11:13 +0800

    setting some tips

--- Project4_2/main.cpp
+++ Project4_2/main.cpp
@@ -15,9 +15,12 @@ int strcmp(char * p1,char * p2){
 }
 int main() {
     char s1[20],s2[20];
+    cout << "First string" <<endl;
     cin >> s1;
+    cout << "Second string" <<endl;
     cin >> s2;
     int t = strcmp(s1,s2);
+    cout << "Comparing~~~~~" << endl;
     cout<<t<<endl;
     return 0;
 }
